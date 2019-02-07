3323e7e47e943cb883d4bc941919b204fc71a05f
lloydbriantech <lloydbriantech@outlook.com>
2019-02-06 19:09:26 -0500

    update to include options for multiple file inputs

--- homework1.py
+++ homework1.py
@@ -1,6 +1,11 @@
 """
 comp805
-homework1
+homework1 - a class implementation to illustrate using a dns
+resolver python package that reads through a file containing
+hostnames
+
+Python version: 3.6.6
+Required PIP Packages: homework1_requirements.txt
 
 Lloyd Dagoc
 University of New Hanmpshire
@@ -8,29 +13,44 @@ University of New Hanmpshire
 
 from dns import resolver
 import subprocess
+import sys
 
 
-class homework1():
+class Homework1():
 
-    def executeSampleMethod():
+    def __init__(self, passed_args):
+        self.passed_args = passed_args
+
+    def query_filelist(self):
+        for i in passed_args:
+            self.resolve_hosts(i)
+
+    def resolve_hosts(self, filename):
+        filename = filename
         my_resolver = resolver.Resolver()
         line_list = []
 
         try:
-            with open("dig-urls.txt") as f_handler:
+            with open(filename) as f_handler:
                 for line_in_file in f_handler:
                     line_in_file = line_in_file.rstrip()
 
                     print(line_in_file)
                     dig_info = my_resolver.query(line_in_file, "A")
 
-                    # create dictionary of lists
-                    # dig_container = [i for i in dig_info]
+                    # TODO: create dictionary of lists
                     for data in dig_info:
                         print(data)
 
         except IOError:
-            print("File does not exist")
+            print(filename + ": File does not exist")
 
-    if __name__ == '__main__':
-        executeSampleMethod()
+
+if __name__ == '__main__':
+    passed_args = sys.argv[1:]
+    if len(passed_args) <= 0:
+        print("Specify a filename input")
+        sys.exit
+    else:
+        homework_obj = Homework1(passed_args)
+        homework_obj.query_filelist()
